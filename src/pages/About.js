1a72cd2a729392374b6b70c8a0e518a018916993
Ghina Shour <shourghina@gmail.com>
2025-09-04 00:13:18 +0300

    adding the about section

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <div className="p-6">
+            <h1 className="text-3xl font-bold mb-4">About Us</h1>
+            <p>We are passionate about delivering the best food in town. Fresh, fast, and delicious! </p>
+        </div>
+    );
+}
