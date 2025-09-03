416431deb7d9904f9c935c8cc407eb08f0395d04
Ghina Shour <shourghina@gmail.com>
2025-09-03 23:27:55 +0300

    creating th home page

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+        <div className="p-6 text-center">
+            <h1 className="text-4xl font-bold mb-4">Welcome To Foodie Express</h1>
+            <p className="text-lg text-grey-600">Delicious meals delivered right to your doorstep </p>
+        </div>
+    );
+}
